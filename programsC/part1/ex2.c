b0255b02e12b60e80bd052926ed0db261498bc0e
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2025-10-22 08:51:12 +0200

    -
    Fix syntax errors in average of squares calculation and update print statement.

--- programsC/part1/ex2.c
+++ programsC/part1/ex2.c
@@ -1,5 +1,7 @@
 /* PART 1 - Ex2: Average of squares from 1 y */
+
 #include <stdio.h>
+
 int main() {
     int, y, somme = 0;
     printf("Enter a value for y (maximum): ");
@@ -7,12 +9,13 @@ int main() {
     for (int i = 1; i <= y; i++) {
         somme += i * i;
     }
-    
     printf("Average of squares from 1 to %d is %.2f\n Sum of squares is equal to ", n, somme / y);
 }
 
 /* PART 1 - Ex2: Factorial of a value */
+
 #include <stdio.h>
+
 int main() {
     int factorial, n;
     printf("Enter an int value to get its factorial: ");
