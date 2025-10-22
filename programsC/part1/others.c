df8bdc7332450365192eefba0c7fe0856fdca155
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2025-10-22 19:08:46 +0200

    -

--- programsC/part1/others.c
+++ programsC/part1/others.c
@@ -33,3 +33,28 @@ int main() {
     }
     printf("\n");
 }
+
+/* Average of squares from 1 y */
+#include <stdio.h>
+
+int main() {
+    int, y, somme = 0;
+    printf("Enter a value for y (maximum): ");
+    scanf("%d" y);
+    for (int i = 1; i <= y; i++) {
+        somme += i * i;
+    }
+    printf("Average of squares from 1 to %d is %.2f\n Sum of squares is equal to ", n, somme / y);
+}
+
+/* Factorial of a value */
+#include <stdio.h>
+
+int main() {
+    int factorial, n;
+    printf("Enter an int value to get its factorial: ");
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    printf("Factorial of %d is %d\n", n, factorial);
+}
