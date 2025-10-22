729a2af03fabefce75693150bad762defe1cae4e
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2025-10-22 08:49:31 +0200

    -

---
+++ programsC/part1/ex4.c
@@ -0,0 +1,9 @@
+/* PART 1 - Ex4: Farenheit to Celcius Convertissor */
+#include <stdio.h>
+int main(void) {
+    double temp;
+    printf("Please enter the temperature in °Fahrenheit: ");
+    if (scanf("%lf", &temp) != 1) return 1;
+    temp = (temp - 32) * (5.0 / 9.0);
+    printf("It's %.2lf Celsius!.\n", temp);
+}
