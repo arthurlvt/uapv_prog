da99420d7d451c99e11bef204af4e14772675fce
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2025-10-22 08:52:45 +0200

    -

--- programsC/part1/ex5.c
+++ programsC/part1/ex5.c
@@ -23,3 +23,37 @@ int main() {
     }
     return 0;
 }
+
+/* PART 1 - Ex5: SORTING VALUES A, B, C */
+#include <stdio.h> 
+int main(void) {
+    int a, b, c;
+    printf("Please enter 3 int values (A, B, C): ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b) {
+        if (a > c) {
+            printf("Ascending order: %d, %d, %d\n", a, b > c ? b : c, b > c ? c : b);
+        } else {
+            printf("ascending order: %d, %d, %d\n", c, a, b);
+        }
+    } else {
+        if (b > c) {
+            printf("ascending order: %d, %d, %d\n", b, a > c ? a : c, a > c ? c : a);
+        } else {
+            printf("ascending order: %d, %d, %d\n", c, b, a);
+        }
+    }
+}
+
+/*PART 1 - Ex5: All divisors of an int */
+#include <stdio.h>
+int main(void) {
+    int n;
+    printf("Enter an int: ");
+    if (scanf("%d", &n) != 1) return 1;
+    printf("Divisors %d are: ", n);
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) printf("%d ", i);
+    }
+    printf("\n");
+}
