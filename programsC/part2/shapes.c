f4eadb454406c4752348b110ccbe6200072b9f41
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2025-10-22 19:02:06 +0200

    -
    Added two programs: one to print a triangle of stars and another to print a square of stars with a hollow center.

---
+++ programsC/part2/shapes.c
@@ -0,0 +1,42 @@
+/* n VALUES IN * */
+#include <stdio.h>
+int main(void) {
+    int n;
+    printf("Enter an integer value n: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+/* SQUARE OF n VALUES IN * */
+#include <stdio.h>
+int main(void) {
+    int n;
+    printf("Enter an integer value n: ");
+    scanf("%d", &n);
+    if (n >= 3) {
+        // Top border
+        for (int i = 1; i <= n; i++) {
+            printf("*");
+        }
+        printf("\n");
+
+        // Middle part
+        for (int i = 1; i <= n - 2; i++) {
+            printf("*");
+            for (int j = 1; j <= n - 2; j++) {
+                printf(" ");
+            }
+            printf("*\n");
+        }
+
+        // Bottom border
+        for (int i = 1; i <= n; i++) {
+            printf("*");
+        }
+    }
+}
