e03c6a1b6d8402d72949c562ea59ecff4019ca89
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2025-10-22 08:27:00 +0200

    -

--- programsC/part1/ex2.c
+++ programsC/part1/ex2.c
@@ -23,7 +23,6 @@ int main() {
 }
 
 /* PART 1 - Ex3:  True and False logical assertions */
-// EXERCICE 4 : Assertions logiques vraies / fausses
 #include <stdio.h>
 int main() {
     int average = 10, grade1 = 6, grade2 = 8, age = 15;
@@ -62,5 +61,61 @@ int main() {
         printf("Assertion F true\n");
     } else {
         printf("Assertion F false\n");
+    }
+}
+
+/* PART 1 - Ex4: Farenheit to Celcius Convertissor */
+#include <stdio.h>
+int main(void) {
+    double temp;
+    printf("Please enter the temperature in °Fahrenheit: ");
+    if (scanf("%lf", &temp) != 1) return 1;
+    temp = (temp - 32) * (5.0 / 9.0);
+    printf("It's %.2lf Celsius!.\n", temp);
+}
+
+/* PART 1 - Ex5: BINGO.c!  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void) {
+    srand((unsigned)time(NULL));
+    int randNum = rand() % 10 + 1;
+    int valuser = 0;
+    int counter_tries = 0;
+    while (valuser != randNum) {
+        printf("Please enter an int between 1 and 9: ");
+        if (scanf("%d", &valuser) != 1) return 1;
+        counter_tries++;
+        if (valuser < randNum) {
+            printf("My number is bigger!!\n");
+        } else if (valuser > randNum) {
+            printf("My number is smaller!\n");
+        } else {
+            printf("Congrats! You found %d by %d tries!\n", randNum, counter_tries);
+        }
+    }
+    return 0;
+}
+
+/* EXERCICE 8 - PARTIE B: TRIE DE VALEURS A, B, C */
+
+#include <stdio.h> 
+int main(void) {
+    int a, b, c;
+    printf("Entrez trois valeurs entières (A, B, C): ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a > b) {
+        if (a > c) {
+            printf("L'ordre décroissant est: %d, %d, %d\n", a, b > c ? b : c, b > c ? c : b);
+        } else {
+            printf("L'ordre décroissant est: %d, %d, %d\n", c, a, b);
+        }
+    } else {
+        if (b > c) {
+            printf("L'ordre décroissant est: %d, %d, %d\n", b, a > c ? a : c, a > c ? c : a);
+        } else {
+            printf("L'ordre décroissant est: %d, %d, %d\n", c, b, a);
+        }
     }
 }
