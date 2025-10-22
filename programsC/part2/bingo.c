67312be8f9a8e2131ed247eacd228ad954459c14
ArthurLVT <70861780+arthurlvt@users.noreply.github.com>
2025-10-22 19:05:03 +0200

    -

--- programsC/part2/bingo.c
+++ programsC/part2/bingo.c
@@ -1,4 +1,4 @@
-/* GUESS THE NUMBER (BINGO!) */
+/* GUESS THE NUMBER (BINGO!) - FIRST WAY */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -18,6 +18,30 @@ int main(void) {
             printf("Lower!\n");
         } else {
             printf("Congratulations! You found the number in %d attempts.\n", count);
+        }
+    }
+}
+
+/* GUESS THE NUMBER (BINGO!) - SECOND WAY */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand((unsigned)time(NULL));
+    int randNum = rand() % 10 + 1;
+    int valuser = 0;
+    int counter_tries = 0;
+    while (valuser != randNum) {
+        printf("Please enter an int between 1 and 9: ");
+        if (scanf("%d", &valuser) != 1) return 1;
+        counter_tries++;
+        if (valuser < randNum) {
+            printf("My number is bigger!!\n");
+        } else if (valuser > randNum) {
+            printf("My number is smaller!\n");
+        } else {
+            printf("Congrats! You found %d by %d tries!\n", randNum, counter_tries);
         }
     }
 }
